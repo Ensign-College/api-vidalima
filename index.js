0040b9e219bc8d5958b183c1f6f38becb7229707
vidalima <121983209+vidalima@users.noreply.github.com>
2024-04-03 10:20:21 -0600

    index updates

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const Ajv = require('ajv');
 const ajv = new Ajv();
 
 const redisClient = Redis.createClient({
-    url:`redis://${process.env.REDIS_HOST}:6379`
+    url:`${process.env.REDIS_HOST}:6379`
 });
 
 exports.boxesHandler = async (event, context) => {
